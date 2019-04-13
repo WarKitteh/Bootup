03c7442a6aec9115afaa982b1fc5cd4030b752d6
Andrew <bootupstore@icloud.com>
2019-04-13 09:13:41 -0700

    First Commit

--- Bootup/ViewController.swift
+++ Bootup/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var loginScrollView: UIScrollView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginScrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height+100)
     }
-
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        loginScrollView.setContentOffset(CGPoint (x: 0, y: 150), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        loginScrollView.setContentOffset(CGPoint (x: 0, y: 0), animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        passwordTextField.resignFirstResponder()
+        emailTextField.resignFirstResponder()
+        return true
+        }
 }
+
+
 
